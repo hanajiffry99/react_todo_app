36889f9d99a4cc88750008c39d78a7c2faed1f06
Hana Jiffry <hanajiffryscg@gmail.com>
2021-09-11 00:53:18 +0530

    adding a new row to table

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
 import { Container,Row,Col, Form, Button } from "react-bootstrap";
 import data from "./data.json";
@@ -5,12 +6,41 @@ import "./Table.css";
 
 function Table(){
 
-    const [tasks,settasks]=useState(data);
+    const [tasks,settasks] = useState(data);
+    const [add,setadd]=useState({
+        date:'',task:'',status:' '
+    });//values used for name tag in form are used here
+
+    const handleadd= (e) =>
+    {
+        e.preventDefault();
+        
+        const fieldname = e.target.getAttribute('name');
+        const newformdata = {...add};
+
+        newformdata[fieldname]=e.target.value;
+        setadd(newformdata);
+    };
+
+    const handleaddsubmit = (e) =>{
+        e.preventDefault();
+
+        const newtask = {
+            id: nanoid(),
+            date : add.date,
+            task : add.task,
+            status : add.status,
+        };
+
+        const newtasks = [...tasks,newtask];
+        settasks(newtasks);
+    };
 
     return(
         <>
         <h1>This is Todo page</h1>
         <Container>
+        <h2>Todo List</h2>
             <table>
                 <thead>
                     <th>Date</th>
@@ -24,19 +54,21 @@ function Table(){
                         <td>{task.date}</td>
                         <td>{task.task}</td>
                         <td>{task.status}</td>
+                        <td><Button>Add</Button>  <Button>Delete</Button>  <Button>Edit</Button></td>
                     </tr>
                     ))}
                 </tbody>
             </table>
         </Container>
+        <hr/><h2>Adding a task</h2>
         <Form>
             <label>Date</label><br/>
-            <input type="datetime-local" placeholder="Enter the date"/><br/><br/>
+            <input type="date" name="date" placeholder="Enter the date" required onChange={handleadd}/><br/><br/>
             <label>Task</label><br/>
-            <input type="text" placeholder="Enter the task"/><br/><br/>
+            <input type="text" name="task" placeholder="Enter the task" required onChange={handleadd}/><br/><br/>
             <label>Status</label><br/>
-            <input type="text" placeholder="Enter the status"/><br/><br/>
-            <Button >Add Task</Button>
+            <input type="text" name="status" placeholder="Enter the status" required onChange={handleadd}/><br/><br/>
+            <Button onClick={handleaddsubmit}>Add Task</Button>
         </Form>
         </>
     )
