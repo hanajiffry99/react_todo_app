2ab2d14de2b938825ae9fc4508a8d811507fd95e
Hana Jiffry <hanajiffryscg@gmail.com>
2021-09-10 23:51:10 +0530

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
+import Table from "./Components/Table/Table";
+import Home from "./Components/Home/Home";
+import Post from "./Components/Post/Post";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component //app is the js file name
 {
   render()
   {
-    return <h1>Hello</h1>;
+    return(
+      <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/post' component={Post}/>
+          <Route path='/todo' component={Table}/>
+        </Switch>
+      </Router>
+      </>
+    );
   }
 }
 
